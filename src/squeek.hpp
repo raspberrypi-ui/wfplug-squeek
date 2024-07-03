03d2d020950d073bc1e9933bb41a9efb8a5686b0
Simon Long <simon@raspberrypi.com>
2024-07-03 08:19:15 +0100

    Remove outdated code

--- src/squeek.hpp
+++ src/squeek.hpp
@@ -14,10 +14,6 @@ class WayfireSqueek : public WayfireWidget
     WfOption <std::string> bar_pos {"panel/position"};
     sigc::connection icon_timer;
 
-    static constexpr conf_table_t conf_table[1] = {
-        {CONF_NONE, NULL,       NULL}
-    };
-
   public:
 
     void init (Gtk::HBox *container) override;
@@ -26,8 +22,6 @@ class WayfireSqueek : public WayfireWidget
     bool set_icon (void);
     void on_button_press_event (void);
     void do_menu (void);
-    static const char *display_name (void) { return N_("Squeekboard"); };
-    static const conf_table_t *config_params (void) { return conf_table; };
 };
 
 #endif /* end of include guard: WIDGETS_SQUEEK_HPP */
