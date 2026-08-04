ac83ca60a87c19c941feebdf7d013e42da3db348
Simon Long <simon@raspberrypi.com>
2026-08-04 14:11:38 +0100

    Tidying

--- src/squeek.cpp
+++ src/squeek.cpp
@@ -33,7 +33,7 @@ extern "C" {
     void destroy (PanelWidget *w) { delete w; }
 
     static constexpr conf_table_t conf_table[1] = {
-        {CONF_TYPE_NONE, NULL, NULL, NULL}
+        {CONF_TYPE_NONE, NULL, NULL, NULL, NULL}
     };
     const conf_table_t *config_params (void) { return conf_table; };
     const char *display_name (void) { return PLUGIN_TITLE; }
